1ec891d1c6db4cae8bf8ff87b38a4188034c8125
dexmcmillan <dexmcmillan@gmail.com>
2022-06-02 08:34:52 -0400

    oops don't worry about it

--- tests/test_datawrappergraphics.py
+++ tests/test_datawrappergraphics.py
@@ -7,7 +7,7 @@ def test_map():
     
     raw = pd.DataFrame({"title": ["Point 1"], "latitude": [50.2373819], "longitude": [-90.708556], "anchor": ["middle-right"], "tooltip": ["A test tooltip."], "icon": ["attention"]})
     
-    assert (Map(chart_id=TEST_CHART_ID, auth_token="HQd4kFqSSQyh5I7dK7YKNonyG74UpldKls1KOShOWxZNbpZyrbgO1kNArRAfwW3I")
+    assert (Map(chart_id=TEST_CHART_ID)
         .data(raw)
         .head(f"Testing datawrappergraphics library")
         .deck("")
