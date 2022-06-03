7f8a8adfaf7263fdd69422ce071031342771f1e5
dexmcmillan <dexmcmillan@gmail.com>
2022-06-03 17:29:56 -0400

    new dist

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(include=["datawrappergraphics"]),
     include_package_data = True,
     package_data={'datawrappergraphics': ['assets/*.json']},
-    version='0.2.36',
+    version='0.2.37',
     description='A pythonic representation of Datawrapper graphics.',
     author='Dexter McMillan',
     license='MIT',
